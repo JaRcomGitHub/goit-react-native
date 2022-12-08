fb11faa3066e3ab9c2ef7ecdac9cacfc5815421c
JaRcomGitHub <jarcom@ukr.net>
2022-12-08 10:06:14 +0200

    dz2

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from "react";
+// import React, { useEffect } from "react";
+// import {} from "react-native";
+import LoginScreen from "./Screens/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+// import * as Font from "expo-font";
+// import AppLoading from 'expo-app-loading';//import { AppLoading } from "expo";
+
+// const loadApplication = async () => {
+//   await Font.loadAsync({
+//     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+//   });
+// };
 
 export default function App() {
+  const [isLogin, setIsLogin] = useState(true);
+  // const [iasReady, setIasReady] = useState(false);
+
+  const handlerSwitchLoginRegScr = () => {
+    setIsLogin(!isLogin);
+    console.log(isLogin);
+  }
+
+  // if (!iasReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadApplication}
+  //       onFinish={() => setIasReady(true)}
+  //       onError={console.warn}
+  //     />
+  //   );
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>goit-react-native</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {isLogin && <LoginScreen onSwitch={ handlerSwitchLoginRegScr } />}
+      {!isLogin && <RegistrationScreen onSwitch={ handlerSwitchLoginRegScr } />}
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
