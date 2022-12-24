8d2cdd5a390ca8be8b76cdea486b5bf1249aef44
JaRcomGitHub <jarcom@ukr.net>
2022-12-24 16:33:26 +0200

    work

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+// import * as firebase from "firebase";
+// import "firebase/auth";
+// import "firebase/storage";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC8gndAWuJes3Y9J5ZWHH67tmDQs4YaFx4",
+//   authDomain: "react-native-jarcom.firebaseapp.com",
+//   projectId: "react-native-jarcom",
+//   storageBucket: "react-native-jarcom.appspot.com",
+//   messagingSenderId: "133086671079",
+//   appId: "1:133086671079:web:209b31114010823990f242",
+//   measurementId: "G-4L9KYLZV51",
+// };
+
+// export default firebase.initializeApp(firebaseConfig);
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,11 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
+// import db from "../firebase/config";
 
 const initialAuthState = {
   login: "",
   email: "",
   password: "",
   isAuth: false,
+
+  userId: null,
+  nickName: null,
+  stateChange: false,
 };
 
 export const authSlice = createSlice({
@@ -35,10 +39,85 @@ export const authSlice = createSlice({
       state.password = "";
       state.isAuth = false;
     },
+    // updateUserProfile: (state, { payload }) => ({
+    //   ...state,
+    //   userId: payload.userId,
+    //   nickName: payload.nickName,
+    // }),
+    // authStateChange: (state, { payload }) => ({
+    //   ...state,
+    //   stateChange: payload.stateChange,
+    // }),
+    // authSignOut: () => state,
   },
 });
 
 export const { logIn, regIn, logOut } = authSlice.actions;
 export const selectIsAuth = (store) => store.auth.isAuth;
-export const selectLogin = (store) => store.auth.login;
-export const selectEmail = (store) => store.auth.email;
+// export const selectLogin = (store) => store.auth.login;
+// export const selectEmail = (store) => store.auth.email;
+
+// const { updateUserProfile, authStateChange, authSignOut } = authSlice.actions;
+
+// export const authSignUpUser =
+//   ({ email, password, nickname }) =>
+//   async (dispatch, getState) => {
+//     try {
+//       await db.auth().createUserWithEmailAndPassword(email, password);
+
+//       const user = await db.auth().currentUser;
+
+//       // if (user) {
+//       await user.updateProfile({
+//         displayName: nickname,
+//         // displayName: "Bob",
+//         // photoURL: "https://example.com.jpg",
+//       });
+//       // }
+
+//       const { displayName, uid } = await db.auth().currentUser;
+
+//       const userUpdateProfile = {
+//         nickName: displayName,
+//         userId: uid,
+//       };
+
+//       dispatch(updateUserProfile(userUpdateProfile));
+//     } catch (error) {
+//       console.log("error", error);
+
+//       console.log("error.message", error.message);
+//     }
+//   };
+
+// export const authSignInUser =
+//   ({ email, password }) =>
+//   async (dispatch, getState) => {
+//     try {
+//       const user = await db.auth().signInWithEmailAndPassword(email, password);
+//       console.log("user", user);
+//     } catch (error) {
+//       console.log("error", error);
+//       console.log("error.code", error.code);
+//       console.log("error.message", error.message);
+//     }
+//   };
+
+// export const authSignOutUser = () => async (dispatch, getState) => {
+//   await db.auth().signOut();
+//   dispatch(authSignOut());
+// };
+
+// export const authStateCahngeUser = () => async (dispatch, getState) => {
+//   await db.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       const userUpdateProfile = {
+//         nickName: user.displayName,
+//         userId: user.uid,
+//       };
+
+//       dispatch(authStateChange({ stateChange: true }));
+//       dispatch(updateUserProfile(userUpdateProfile));
+//     }
+//   });
+// };
